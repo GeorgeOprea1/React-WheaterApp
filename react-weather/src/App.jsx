250f9793890e065ee7e01dd97900df1879522c6c
GeorgeOprea <oprea.george14@yahoo.com>
2024-02-09 11:33:56 +0200

    initial setup

---
+++ react-weather/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-[#1f213a] h-screen flex justify-center align-top">
+        <div className="bg-blue-200 mt-40 w-1/5 h-1/3"></div>
+        <div className="bg-blue-400 mt-40 w-1/3 h-1/3"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
